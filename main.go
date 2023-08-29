6f2c64e8f03a6f3697012c6a8e9204f01193b7bf
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-29 13:38:37 +0200

    Add command line output

--- main.go
+++ main.go
@@ -1,119 +1,121 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "strings"
-    "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 // Repository represents a GitHub repository
 type Repository struct {
-    Name  string `json:"name"`
-    Stars int    `json:"stargazers_count"`
+	Name  string `json:"name"`
+	Stars int    `json:"stargazers_count"`
 }
 
 // Fetch the list of repositories for a given GitHub user
 func fetchRepositories(username string) ([]Repository, error) {
-    var repos []Repository
-    var nextPageURL string = "https://api.github.com/users/" + username + "/repos?per_page=100"
+	var repos []Repository
+	var nextPageURL string = "https://api.github.com/users/" + username + "/repos?per_page=100"
 
-    for nextPageURL != "" {
-        // Fetch the next page of repositories
-        resp, err := http.Get(nextPageURL)
-        if err != nil {
-            return nil, err
-        }
-        defer resp.Body.Close()
+	for nextPageURL != "" {
+		// Fetch the next page of repositories
+		resp, err := http.Get(nextPageURL)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
 
-        // Read the response body
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            return nil, err
-        }
+		// Read the response body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
 
-        // Unmarshal the JSON response into a slice of Repository
-        var pageRepos []Repository
-        if err := json.Unmarshal(body, &pageRepos); err != nil {
-            return nil, err
-        }
+		// Unmarshal the JSON response into a slice of Repository
+		var pageRepos []Repository
+		if err := json.Unmarshal(body, &pageRepos); err != nil {
+			return nil, err
+		}
 
-        // Append the repositories to the repos slice
-        repos = append(repos, pageRepos...)
+		// Append the repositories to the repos slice
+		repos = append(repos, pageRepos...)
 
-        // Get the next page URL from the Link header
-        nextPageURL = getNextPageURL(resp.Header.Get("Link"))
+		fmt.Printf("got %d repos so far\n", len(repos))
 
-        // Wait for 1 minute before fetching the next page
-        time.Sleep(1 * time.Minute)
-    }
+		// Get the next page URL from the Link header
+		nextPageURL = getNextPageURL(resp.Header.Get("Link"))
 
-    return repos, nil
+		// Wait for 1 minute before fetching the next page
+		time.Sleep(1 * time.Minute)
+	}
+
+	return repos, nil
 }
 
 // Get the next page URL from the Link header
 func getNextPageURL(linkHeader string) string {
-    if linkHeader == "" {
-        return ""
-    }
+	if linkHeader == "" {
+		return ""
+	}
 
-    links := strings.Split(linkHeader, ",")
-    for _, link := range links {
-        parts := strings.Split(link, ";")
-        if len(parts) == 2 && strings.TrimSpace(parts[1]) == `rel="next"` {
-            return strings.TrimSpace(parts[0])[1 : len(parts[0])-2]
-        }
-    }
+	links := strings.Split(linkHeader, ",")
+	for _, link := range links {
+		parts := strings.Split(link, ";")
+		if len(parts) == 2 && strings.TrimSpace(parts[1]) == `rel="next"` {
+			return strings.TrimSpace(parts[0])[1 : len(parts[0])-2]
+		}
+	}
 
-    return ""
+	return ""
 }
 
 // Get the total star count for a list of repositories
 func getTotalStars(repos []Repository) int {
-    totalStars := 0
-    for _, repo := range repos {
-        totalStars += repo.Stars
-    }
-    return totalStars
+	totalStars := 0
+	for _, repo := range repos {
+		totalStars += repo.Stars
+	}
+	return totalStars
 }
 
 func main() {
-    username := "xyproto"
-    if len(os.Args) > 1 {
-        username = os.Args[1]
-    }
+	username := "xyproto"
+	if len(os.Args) > 1 {
+		username = os.Args[1]
+	}
 
-    var currentStars int
+	var currentStars int
 
-    // Fetch the list of repositories
-    repos, err := fetchRepositories(username)
-    if err != nil {
-        panic(err)
-    }
+	// Fetch the list of repositories
+	repos, err := fetchRepositories(username)
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Printf("got %d repos\n", len(repos))
+	fmt.Printf("got %d repos\n", len(repos))
 
-    for {
-        // Get the total star count
-        totalStars := getTotalStars(repos)
+	for {
+		// Get the total star count
+		totalStars := getTotalStars(repos)
 
-        fmt.Printf("got %d total stars\n", totalStars)
+		fmt.Printf("got %d total stars\n", totalStars)
 
-        // If this is the first iteration, initialize currentStars
-        if currentStars == 0 {
-            currentStars = totalStars
-        }
+		// If this is the first iteration, initialize currentStars
+		if currentStars == 0 {
+			currentStars = totalStars
+		}
 
-        // If the star count has increased, print "STAR!" and update currentStars
-        if totalStars > currentStars {
-            fmt.Println("STAR!")
-            currentStars = totalStars
-        }
+		// If the star count has increased, print "STAR!" and update currentStars
+		if totalStars > currentStars {
+			fmt.Println("STAR!")
+			currentStars = totalStars
+		}
 
-        // Wait for 10 minutes before checking again
-        time.Sleep(10 * time.Minute)
-    }
+		// Wait for 10 minutes before checking again
+		time.Sleep(10 * time.Minute)
+	}
 }
